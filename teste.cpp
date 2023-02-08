656dd72a8071d69355e3fe7c186071f348faa732
Rodrigo Martins <rodrimartins2005@gmail.com>
2023-02-08 08:54:53 -0300

    teste 1 2023

---
+++ teste.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int idade;
+    cout << "Entre sua idade: ";
+    cin >> idade;
+    cout << "Voce tem " << idade << " anos." << endl;
+    return 0;
+}
+
+
+
+
+
